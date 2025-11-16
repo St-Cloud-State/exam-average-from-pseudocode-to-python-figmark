6888d61e9c122ed726ab98607baa90fe602c97de
figmark <diomarkadon@gmail.com>
2025-11-16 09:33:01 -0600

    Added Comments to Code

--- main.py
+++ main.py
@@ -15,16 +15,23 @@ Pseudocode
 7. Print the final average score on its own line.
 8. End the program.
 """
-
+#Define the function
 def calculate_average():
-
+# Prompt user to enter first score as integer
     first_score = int(input("Enter first exam score: "))
+# Prompt user to enter second score as integer
     second_score = int(input("Enter second exam score: "))
+# Prompt user to enter third score as integer
     third_score = int(input("Enter third exam score: "))
+# Calculate the average score
     average_score = (first_score + second_score + third_score) / 3
+# Print first score back for user to see
     print("First score:", first_score)
+# Print second score back for user to see
     print("Second score:", second_score)
+# Print third score back for user to see
     print("Third score:", third_score)
+# Print the average score
     print("The average score is:", average_score)
+# Call the function
 calculate_average()
-
